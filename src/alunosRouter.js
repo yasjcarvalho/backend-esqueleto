eebb955f7b253463d8c7342472c9706342659bb5
yasjcarvalho <yasmin.carvalho@igdema.ufal.br>
2023-05-31 16:35:47 -0300

    Update alunosRouter.js

--- src/alunosRouter.js
+++ src/alunosRouter.js
@@ -1,48 +1,41 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
-router.use(express.json());
+const router = express.Router()
+router.use(express.json())
 
 router.get('/', async (req, res) => {
-    res.send({ "message": "todos os alunos" });
-});
+  res.send({ message: 'todos os alunos' })
+})
 
 router.get('/:id', async (req, res) => {
-
-    res.send({});
-});
+  res.send({})
+})
 
 router.get('/matricula/:matricula', async (req, res) => {
-    res.send({});
-});
+  res.send({})
+})
 
-router.post('/', async (req, res) => {//salvar
-    res.send({
-    });
-});
+router.post('/', async (req, res) => {
+  //salvar
+  res.send({})
+})
 
-router.put('/:id', async (req, res) => {//atualizar
-    res.send({
+router.put('/:id', async (req, res) => {
+  //atualizar
+  res.send({})
+})
 
-    });
-});
-
-router.delete('/:id', async (req, res) => {//delete
-    res.status(204).send({
-
-    });
-});
+router.delete('/:id', async (req, res) => {
+  //delete
+  res.status(204).send({})
+})
 
 router.post('/:id/atividade', async (req, res) => {
-    res.send({
-
-    });
-});
+  res.send({})
+})
 
 router.put('/:id/atividade/:idAtividade', async (req, res) => {
-    res.send({
+  res.send({})
+})
 
-    });
-});
-
-module.exports = router;
+module.exports = router
