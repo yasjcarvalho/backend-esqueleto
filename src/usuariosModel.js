bd032fce43694251dd82ae5e7bd397f633a1b3e4
yasjcarvalho <yasmin.carvalho@igdema.ufal.br>
2023-06-01 14:32:18 -0300

    ola

--- src/usuariosModel.js
+++ src/usuariosModel.js
@@ -1,2 +1,26 @@
 const { Sequelize, DataTypes } = require('sequelize')
 const db = require('./db')
+
+const Usuario = db.define(
+  'Usuario',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nome: DataTypes.STRING(100),
+    email: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: false
+    },
+    senha: {
+      type: DataTypes.STRING(20),
+      allowNull: false
+    }
+  },
+  {}
+)
+
+module.exports - Usuario
